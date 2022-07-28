9b2aa73e20cf7073c2f5a3647f32484aa214e1c9
VarnikaB <1rn18cs115.varnika@gmail.com>
2022-07-28 13:56:12 +0530

    All four balls are moving at respective position and pace

--- src/main/java/TryProcessing.java
+++ src/main/java/TryProcessing.java
@@ -3,8 +3,11 @@ public class TryProcessing extends PApplet {
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
     public static final int RGB1 = 255;
-    public static final int DIAMETER = 100;
-    int x = 0;
+    public static final int DIAMETER = 10;
+    int circlex1 = 0;
+    int circlex2 = 0;
+    int circlex3 = 0;
+    int circlex4 = 0;
 
     public static void main(String[] args){
         PApplet.main("TryProcessing", args);
@@ -18,24 +21,24 @@ public class TryProcessing extends PApplet {
 
     @Override
     public void setup() {
-        //System.out.println("Setup method");
-        drawCircle(WIDTH / 2); // width: xpos, height: ypos, c = widthOfEllipse, d = heightOfEllipse
+        drawCircle(); // width: xpos, height: ypos, c = widthOfEllipse, d = heightOfEllipse
     }
 
     @Override
     public void draw() {
-        //System.out.println("Draw Method");
-        //mouseX represents the current position of mouse in X direction
-        //mouseY represents the current position of mouse in Y direction
-        paintWhite();  //This will keep replacing the background to white
-        drawCircle(mouseX);
+        drawCircle();
     }
 
-    private void drawCircle(int mouseX) {
-        ellipse(x, HEIGHT / 2, DIAMETER, DIAMETER);
-        x++;
-        if(x == WIDTH)
-            x = 0;
+    private void drawCircle() {
+        ellipse(circlex1, HEIGHT / 5, DIAMETER, DIAMETER);
+        circlex1++;
+        ellipse(circlex2, HEIGHT * 2 / 5, DIAMETER, DIAMETER);
+        circlex2 += 2;
+        ellipse(circlex3, HEIGHT * 3 / 5, DIAMETER, DIAMETER);
+        circlex3 += 3;
+        ellipse(circlex4, HEIGHT * 4 / 5, DIAMETER, DIAMETER);
+        circlex4 += 4;
+
     }
 
     private void paintWhite() {
