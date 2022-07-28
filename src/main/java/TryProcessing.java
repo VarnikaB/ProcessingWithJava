7f7bf69e4abf501e910e2138aa676c5cc52d63cb
VarnikaB <1rn18cs115.varnika@gmail.com>
2022-07-28 13:27:27 +0530

    Circle crawl according to mouse movement| Size of circle can be changed by varying the c and d parameter

--- src/main/java/TryProcessing.java
+++ src/main/java/TryProcessing.java
@@ -1,5 +1,8 @@
 import processing.core.PApplet;
 public class TryProcessing extends PApplet {
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+
     public static void main(String[] args){
         PApplet.main("TryProcessing", args);
     }
@@ -7,11 +10,20 @@ public class TryProcessing extends PApplet {
     @Override
     public void settings() {
         super.settings();
-        size(640, 480);
+        size(WIDTH, HEIGHT);
     }
 
     @Override
     public void setup() {
-        ellipse(320, 240, 100, 100);
+        //System.out.println("Setup method");
+        ellipse(WIDTH / 2, HEIGHT / 2, 100, 100); // width: xpos, height: ypos, c = widthOfEllipse, d = heightOfEllipse
+    }
+
+    @Override
+    public void draw() {
+        //System.out.println("Draw Method");
+        //mouseX represents the current position of mouse in X direction
+        //mouseY represents the current position of mouse in Y direction
+        ellipse(mouseX, mouseY, 100, 100);
     }
 }
